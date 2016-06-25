0e71f655f095d7c9af707c89cebb13576cdd7eb0
Denima de Moraes <denimar@gmail.com>
2016-06-25 16:48:14 -0300

    Update SampleController.java

--- src/main/java/com/denimar/denienglishsrv/controller/SampleController.java
+++ src/main/java/com/denimar/denienglishsrv/controller/SampleController.java
@@ -10,7 +10,7 @@ public class SampleController {
     @RequestMapping("/hello")
     @ResponseBody
     String home() {
-        return "Hello World 888 !";
+        return "Hello World 999 !";
     }	
 
 }
